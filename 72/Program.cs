314474ba225c406b9eb3570706c2177c2a0f999a
geekbrains <info@geekbrains.ru>
2022-07-13 23:14:36 +0400

    edit

--- 72/Program.cs
+++ 72/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1};
+﻿// Мой вариант
+/*
+int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1};
 int[] info = {2, 3, 3, 1};
 int[] answer = new int[info.Length];
 int dataBitNumber = 0;
@@ -30,6 +32,63 @@ PrintOneDimArray(answer);
 Console.WriteLine();
 
 void PrintOneDimArray(int[] a)
+{
+    Console.Write("[");
+    for(int i = 0; i < a.Length; i++)
+    {
+        Console.Write(a[i]);
+        if(i < a.Length - 1)
+            Console.Write(", ");
+        else
+            Console.WriteLine("]");
+    }
+}
+*/
+
+
+
+
+// Вариант от преподавателя
+
+int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1};
+int[] info = {2, 3, 3, 1};
+int[] answerDecimalNumbers = GetNumbersFromDataInfo(data, info);
+
+Console.Write("data = ");
+PrintArray(data);
+
+Console.Write("info = ");
+PrintArray(info);
+
+Console.Write("result = ");
+PrintArray(answerDecimalNumbers);
+
+int GetDecimal(int[] bits, int minIdx, int maxIdx)
+{
+    int number = 0;
+    for(int i = minIdx; i < maxIdx + 1; i++)
+    {
+        number += bits[i] * (int)Math.Pow(2, maxIdx - i);
+    }
+    return number;
+}
+
+int[] GetNumbersFromDataInfo(int[] bits, int[] sizes)
+{
+    int[] numbers = new int[sizes.Length];
+    int startIdx = 0;
+    int endIdx = -1;
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        startIdx = endIdx + 1;
+        endIdx = endIdx + sizes[i];
+        int number = GetDecimal(bits, startIdx, endIdx);
+        numbers[i] = number;
+    }
+    return numbers;
+}
+
+void PrintArray(int[] a)
 {
     Console.Write("[");
     for(int i = 0; i < a.Length; i++)
